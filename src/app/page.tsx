79b9fa677447aff47318e36c0237a5c663b3e3d6
VijaiMegala <dr.vijaianand07@gmail.com>
2025-02-25 09:38:25 +0530

    render bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
             setTasks(response.data);
         }
         fetchTasks();
-    }, []);
+    }, [token]);
 
     const handleView = (id: string) => {
         router.push(`/view/${id}`);
